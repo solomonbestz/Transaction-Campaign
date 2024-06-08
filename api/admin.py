b71ab378e65e55597675472fcb9ac1c08adf0be7
Solomon Bestz <kingsolodawarrior@gmail.com>
2024-06-08 23:27:08 +0100

    Admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Wallet, Campaign,Transaction
+
+
+admin.site.register(Wallet)
+admin.site.register(Campaign)
+admin.site.register(Transaction)
