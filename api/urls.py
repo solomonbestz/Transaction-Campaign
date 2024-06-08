6d23c2635d69fa0aba572cb33ee1c33928cde3a0
Solomon Bestz <kingsolodawarrior@gmail.com>
2024-06-08 23:27:17 +0100

    Api

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 
 from . import views
 
+app_name = 'api'
 
 urlpatterns =[
     # Authentication route
@@ -11,5 +12,17 @@ urlpatterns =[
     path("auth/login/", LoginView.as_view(), name="rest_login"),
     path("auth/logout/", LogoutView.as_view(), name="rest_logout"),
     path("auth/user/", UserDetailsView.as_view(), name="rest_user_details"),
+
+    # wallets ENDPOINT
+    path("wallets/", views.WalletList.as_view()),
+    path("wallets/<str:uid>/", views.WalletDetail.as_view()),
+
+    # campaigns ENDPOINT
+    path("campaigns/", views.CampaignList.as_view()),
+    path("campaigns/<str:uid>/", views.CampaignDetail.as_view()),
+
+    # transactions ENDPOINT
+    path("transactions/",  views.TransactionList.as_view()),
+    path("transsactions/<str:uid>/", views.TransactionDetail.as_view()),
 
 ]
